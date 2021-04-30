f64897ec61585e1c5a20e75892351b53c194b190
BalGrymos <kevin_ber_69@hotmail.fr>
2021-04-30 15:46:17 +0200

    menu deroulant pour les couleurs en JS

--- product.js
+++ product.js
@@ -1,34 +1,22 @@
+
 (async function() {
     const articles = await getArticles()
+    displayTargetedArticle(articles)
 
-    for (article of articles){
-        if(getId() === article._id){
-            displayTargetedArticle()
-        }
-    }
-
-    for (article of articles){
-        if(article.colors != undefined){
-            console.log(article.colors)
-            displayColorArticle() 
-        }
-    }
+    //Implémente les valeurs de l'array couleurs de l'article dans le HTML
+    const colors = articles.colors;
+    colors.forEach((element, i) =>{
+        document.getElementById('color').innerHTML +=`
+        <option value="${i+1}">${element}</option>`
+    });
+    
+    // for (let article of Object.keys(articles.colors)){
+    //      displayColorArticle(article)
+    //      console.log(article)
+    // }
 })()
 
-function getArticles(){
-    return fetch("http://localhost:3000/api/teddies/")
-        .then(function(response){
-           return response.json()
-        })
-        .then(function(articles){
-            //console.log(articles)
-            return articles
-        })
-        .catch(function(err){
-            alert(err)
-        })
-}
-
+// Recuperation de l'ID dans l'URL 
 function getId(){
     const queryString = window.location.search;
     const urlParams = new URLSearchParams (queryString)
@@ -36,15 +24,31 @@ function getId(){
     return id;
 }
 
-function displayTargetedArticle() {
+//Fetch de l'API ours en peluche 
+function getArticles(){
+    return fetch(`http://localhost:3000/api/teddies/` + getId())
+        .then(function(response){
+           return response.json()
+        })
+        .then(function(articles){
+            console.log(articles) 
+            return articles
+        })
+        .catch(function(err){
+            alert(err)
+        })
+}
+
+//implémente le produit cliqué en fonction de l'ID en HTML
+function displayTargetedArticle(articles) {
     document.getElementById('target_article').innerHTML += `
     <div class="col d-flex flex-column flex-md-row" id="">
-        <img src="${article.imageUrl}" class="w-50">
+        <img src="${articles.imageUrl}" class="w-50">
         <div class="card">
             <div class="card-body">
-                <h5 class="card-title"><a href="product.html?id=${article._id}">Ours en peluche - ${article.name} </a> </h5>
-                <p class="card-text ">${article.description}</p>
-                <span class="h5">${article.price / 100}€</span>
+                <h5 class="card-title"><a href="product.html?id=">Ours en peluche - ${articles.name} </a> </h5>
+                <p class="card-text ">${articles.description}</p>
+                <span class="h5">${articles.price / 100}€</span>
                 <div>
                     <label for="color">Couleurs</label>
                     <select class="form-select" aria-label="Default select example" name="color" id="color">
@@ -56,14 +60,8 @@ function displayTargetedArticle() {
     </div> `
 }
 
-function displayColorArticle() {
+//Implémente dans le menu déroulant les différentes couleurs dans le HTML
+function displayColorArticle(article) {
     document.getElementById('color').innerHTML +=`
-    <option value="1">${article.colors}</option>
-    
-    `
+    <option value="1">${article.colors}</option>`
 }
-
-// <option value="1">${article.color}</option>
-// <option value="1">${article.color}</option>
-// <option value="1">${article.color}</option>
-// <option value="1">${article.color}</option>
