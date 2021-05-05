3a586cc9f5fdb2a5c8217c3ee1bba502b1eec891
BalGrymos <kevin_ber_69@hotmail.fr>
2021-05-05 15:00:09 +0200

    ajustement product.js + commencement du addEventListener sur le bouton

--- product.js
+++ product.js
@@ -1,21 +1,3 @@
-
-(async function() {
-    const articles = await getArticles()
-    displayTargetedArticle(articles)
-
-    //Implémente les valeurs de l'array couleurs de l'article dans le HTML
-    const colors = articles.colors;
-    colors.forEach((element, i) =>{
-        document.getElementById('color').innerHTML +=`
-        <option value="${i+1}">${element}</option>`
-    });
-    
-    // for (let article of Object.keys(articles.colors)){
-    //      displayColorArticle(article)
-    //      console.log(article)
-    // }
-})()
-
 // Recuperation de l'ID dans l'URL 
 function getId(){
     const queryString = window.location.search;
@@ -25,14 +7,14 @@ function getId(){
 }
 
 //Fetch de l'API ours en peluche 
-function getArticles(){
-    return fetch(`http://localhost:3000/api/teddies/` + getId())
+function getArticle(){
+     fetch(`http://localhost:3000/api/teddies/` + getId())
         .then(function(response){
            return response.json()
         })
-        .then(function(articles){
-            console.log(articles) 
-            return articles
+        .then(function(article){
+            // console.log(article) 
+            displayTargetedArticle(article);
         })
         .catch(function(err){
             alert(err)
@@ -40,28 +22,39 @@ function getArticles(){
 }
 
 //implémente le produit cliqué en fonction de l'ID en HTML
-function displayTargetedArticle(articles) {
+function displayTargetedArticle(article) {
     document.getElementById('target_article').innerHTML += `
     <div class="col d-flex flex-column flex-md-row" id="">
-        <img src="${articles.imageUrl}" class="card-img-size">
+        <img src="${article.imageUrl}" class="card-img-size">
         <div class="card">
             <div class="card-body">
-                <h5 class="card-title"><a href="product.html?id=">Ours en peluche - ${articles.name} </a> </h5>
-                <p class="card-text ">${articles.description}</p>
-                <span class="h5">${articles.price / 100}€</span>
+                <h5 class="card-title"><a href="product.html?id=">Ours en peluche - ${article.name} </a> </h5>
+                <p class="card-text ">${article.description}</p>
+                <span class="h5">${article.price / 100}€</span>
                 <div>
                     <label for="color">Couleurs</label>
                     <select class="form-select" aria-label="Default select example" name="color" id="color">
                     </select>
                 </div>
-                <button type="button" class="btn btn-primary">Ajoutez à votre panier</button>
+                <button type="button" class="btn btn-primary" id="buttonShopping">Ajoutez à votre panier</button>
             </div>
         </div>
     </div> `
+
+    //Implémente les différentes couleurs du produit en HTML
+    const colors = article.colors;
+    colors.forEach((element, i) =>{
+        document.getElementById('color').innerHTML +=`
+        <option value="${i+1}">${element}</option>`
+    });
 }
 
-//Implémente dans le menu déroulant les différentes couleurs dans le HTML
-function displayColorArticle(article) {
-    document.getElementById('color').innerHTML +=`
-    <option value="1">${article.colors}</option>`
+getArticle()
+    
+var test = document.getElementById('test');
+
+test.addEventListener('click', sayHello);
+
+function sayHello(){
+  console.log('hello');
 }
